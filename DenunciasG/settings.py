3c7cfb6de49ec04fd31afbab31b27bfc41ae6cb5
SantiagoAguila <cristianaguilac08@gmail.com>
2025-07-14 17:22:41 -0500

    ddd

--- DenunciasG/settings.py
+++ DenunciasG/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -150,6 +153,3 @@ EMAIL_HOST_USER = 'tuhackerfav9@gmail.com'
 EMAIL_HOST_PASSWORD = 'afgr vkhk vofc hcyx'
 
 #--------------------------------------------------------
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
