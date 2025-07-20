b7c1fb5f498dae610c924a092fa3c1144c609773
SantiagoAguila <cristianaguilac08@gmail.com>
2025-07-20 13:22:40 -0500

    fto

--- DenunciasG/settings.py
+++ DenunciasG/settings.py
@@ -137,8 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL='/media/'
-MEDIA_ROOT=(os.path.join(BASE_DIR / 'media'))
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 X_FRAME_OPTIONS= 'SAMEORIGIN'
 
--- DenunciasG/urls.py
+++ DenunciasG/urls.py
@@ -18,9 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from Aplicaciones.Usuarios.views import home
 
+#para mostrar los archivos
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'), 
     path('', include('Aplicaciones.Usuarios.urls')),
     path('', include('Aplicaciones.Denuncias.urls')),
 ]
+
+
+if settings.DEBUG: urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
