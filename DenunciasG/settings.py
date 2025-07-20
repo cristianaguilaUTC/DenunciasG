287a69e12d06521b07103986117629d1d5bf0db0
SantiagoAguila <cristianaguilac08@gmail.com>
2025-07-20 16:43:27 -0500

    f

--- DenunciasG/settings.py
+++ DenunciasG/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'DenunciasG/media')
 
 X_FRAME_OPTIONS= 'SAMEORIGIN'
 
